1d1f5f7bef2b30885cb50a8d870dec70e72d4eef
Mohid Tanveer <mohid.tanveer011@gmail.com>
2025-01-30 15:00:33 -0600

    changing to fastapi

--- tubify-core/src/backend/app.py
+++ tubify-core/src/backend/app.py
@@ -1,27 +1,19 @@
-from flask import Flask
+from fastapi import FastAPI
+import uvicorn
 from datetime import timedelta
-from flask_cors import CORS
 from dotenv import load_dotenv
 import os, ssl
 
 # load environment variables
 load_dotenv()
 
-# initialize flask app
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["TOKEN_EXPIRATION"] = timedelta(hours=1)
+# initialize fastapi app
+app = FastAPI()
 
-# enable CORS
-CORS(
-    app,
-    origins=["https://127.0.0.1:3000", "https://localhost:3000"],
-    supports_credentials=True,
-)
 
-# enable SSL
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain("localhost.pem", "localhost-key.pem")
+# enable SSL in fastapi
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(os.getenv("CERT_FILE"), os.getenv("KEY_FILE"))
 
 # import and register blueprints
 # from auth import auth_bp
@@ -31,4 +23,4 @@ context.load_cert_chain("localhost.pem", "localhost-key.pem")
 # app.register_blueprint(spotify_auth_bp)
 
 if __name__ == "__main__":
-    app.run(ssl_context=context, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, ssl_context=context)
