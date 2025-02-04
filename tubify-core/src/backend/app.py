02da1b9c6b2c40b9f3ed79dc7db18521bd0dba15
Mohid Tanveer <mohid.tanveer011@gmail.com>
2025-02-04 17:05:34 -0600

    Add Design Doc

--- tubify-core/src/backend/app.py
+++ tubify-core/src/backend/app.py
@@ -11,15 +11,8 @@ app = FastAPI()
 
 
 # enable SSL in fastapi
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context = ssl.create_default_context(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(os.getenv("CERT_FILE"), os.getenv("KEY_FILE"))
-
-# import and register blueprints
-# from auth import auth_bp
-# from spotify_auth import spotify_auth_bp
-
-# app.register_blueprint(auth_bp)
-# app.register_blueprint(spotify_auth_bp)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, ssl_context=context)
