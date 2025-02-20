6e1ae33747b7c61f6df85dff9b2bb5011407a7e9
Will Foster <willcjfoster@gmail.com>
2025-02-20 14:11:42 -0600

    correct package installment, button fix

--- tubify-core/src/pages/WatchPage.tsx
+++ tubify-core/src/pages/WatchPage.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import { TubifyTitle } from "@/components/ui/tubify-title";
-import YouTube, { YouTubeProps } from "react-youtube";
+import YouTube from "react-youtube";
+import { Button } from "@/components/ui/button";
 
 export default function WatchPage() {
-  const videoId = "dQw4w9WgXcQ"; // Replace with the ID of the YouTube video you want to play
+  const videoId = "dQw4w9WgXcQ";
 
   const opts = {
     height: '390',
@@ -26,11 +27,17 @@ export default function WatchPage() {
       </div>
       <div className="flex-1 flex items-center justify-center">
         <div className="flex flex-col items-center gap-4">
-          <p className="text-white">Welcome to the Watch Page!</p>
+          <h1 className="text-white text-4xl">Welcome to the Watch Page!</h1>
           <YouTube videoId={videoId} opts={opts} onReady={onReady} />
-          <Link to="/" className="text-white hover:text-slate-300">
-            Go back to Homepage
-          </Link>
+          
+          <Button
+              asChild
+              className="bg-black hover:bg-neutral-900 border-slate-800 hover:border-slate-600 hover:text-slate-300 text-white"
+            >
+              <Link to="/" className="text-white hover:text-slate-300">
+                Go back to Homepage
+              </Link>
+          </Button>
         </div>
       </div>
     </div>
